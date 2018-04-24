89ca5b187e0285495e6dd5188291972ed7a36150
Hans-J. Schmid <knock@myopendoor.de>
2018-04-24 02:47:26 +0200

    adds sample code

---
+++ Chapter01/recipe_1_11_2.cpp
@@ -0,0 +1,13 @@
+#include "recipe_1_11_2.h"
+#include <iostream>
+#include <string>
+
+using namespace std::string_literals;
+
+namespace {
+void print(std::string message) { std::cout << "[file2] " << message << std::endl; }
+}  // namespace
+
+namespace recipe_1_11 {
+void file2_run() { print("run"s); }
+}  // namespace recipe_1_11
---
+++ Chapter01/recipe_1_11_2.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace recipe_1_11 {
+void file2_run();
+}
