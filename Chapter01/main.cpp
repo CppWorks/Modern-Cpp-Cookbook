eb8b9b5e7d387491f24ceb4027c354d3de0482bd
Hans-J. Schmid <knock@myopendoor.de>
2018-05-14 20:47:39 +0200

    adds headers.

--- Chapter01/main.cpp
+++ Chapter01/main.cpp
@@ -16,22 +16,35 @@
 
 int main()
 {
+  // Using auto whenever possible
   recipe_1_01::execute();
+  // Creating type aliases and alias templates
   recipe_1_02::execute();
+  // Understanding uniform initialization
   recipe_1_03::execute();
+  // Understanding the various forms of non-static member initialization
   recipe_1_04::execute();
+  // Controlling and querying object alignment
   recipe_1_05::execute();
+  // Using scoped enumerations
   recipe_1_06::execute();
+  // Using override and final for virtual methods
   recipe_1_07::execute();
+  // Using range-based for loops to iterate on a range
   recipe_1_08::execute();
+  // Enabling range-based for loops for custom types
   recipe_1_09::execute();
+  // Using explicit constructors and conversion operators to avoid implicit conversion
   recipe_1_10::execute();
   // call the print functions defined in their translation units
   // Note: the filex_run functions live in the same namespace which does not matter.
+  // Using unnamed namespaces instead of static globals
   recipe_1_11_3::execute();
   recipe_1_11::file1_run();
   recipe_1_11::file2_run();
+  // Using inline namespaces for symbol versioning
   recipe_1_12::client::execute();
+  // Using structured bindings to handle multi-return values
   recipe_1_13::execute();
 
   return 0;
--- Chapter02/main.cpp
+++ Chapter02/main.cpp
@@ -13,10 +13,15 @@
 
 int main()
 {
+  // Converting between numeric and string types
   recipe_2_01::execute();
+  // Limits and other properties of numeric types
   recipe_2_02::execute();
+  // Generating pseudo-random numbers
   recipe_2_03::execute();
+  // Initializing all bits of internal state of a pseudo-random number generator
   recipe_2_04::execute();
+  // 
   recipe_2_05::execute();
   recipe_2_06::execute();
   recipe_2_07::execute();
