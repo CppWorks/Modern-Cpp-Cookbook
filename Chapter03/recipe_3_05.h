64f33fb0187beb088ac01029775a011a5a1ae6c8
Hans-J. Schmid <knock@myopendoor.de>
2018-05-17 02:10:52 +0200

    syncs code with book chapters.

--- Chapter03/recipe_3_05.h
+++ Chapter03/recipe_3_05.h
@@ -1,108 +1,49 @@
 #pragma once
 
-#include "funclib.h"
-#include <algorithm>
-#include <array>
 #include <iostream>
-#include <list>
-#include <map>
-#include <queue>
 #include <string>
-#include <vector>
-#include <cmath>
+
+// Functions with a variable number of arguments are called VARIADIC FUNCTIONS. Function
+// templates with a variable number of arguments are called VARIADIC FUNCTION TEMPLATES.
+
+// In order to write variadic function templates, you must perform the following steps:
+
+// 1. Define an overload with a fixed number of arguments to end compile-time recursion if the semantics of the variadic function template require it (refer to [1] in the following code).
+
+// 2. Define a template parameter pack to introduce a template parameter that can hold any number of arguments, including zero; these arguments can be either types, non-types, or templates (refer to [2]).
+
+// 3. Define a function parameter pack to hold any number of function arguments, including zero; the size of the template parameter pack and the corresponding function parameter pack is the same and can be determined with the sizeof... operator (refer to [3]).
+
+// 4. Expand the parameter pack in order to replace it with the actual arguments being
+// supplied (refer to [4]).
 
 namespace recipe_3_05 {
-  using namespace std::string_literals;
+template <typename T> // [1] overload with fixed number of arguments
+T add(T value)
+{
+  return value;
+}
 
-  template <class T = double>
-  struct fround {
-    typename std::enable_if<std::is_floating_point<T>::value, T>::type
-    operator()(const T& value) const
-    {
-      return std::round(value);
-    }
-  };
+template <typename T, typename... Ts> // [2] typename... Ts
+T add(T head, Ts... rest) // [3] Ts... rest
+{
+  return head + add(rest...); // [4] rest...
+}
 
-  void execute()
-  {
-    {
-      auto vnums = std::vector<int>{ 0, 2, -3, 5, -1, 6, 8, -4, 9 };
-      auto r = funclib::mapf([](int const i) { return std::abs(i); }, vnums);
-      // r = {0, 2, 3, 5, 1, 6, 8, 4, 9}
+void execute()
+{
+  std::cout
+    << "\nRecipe 3.05: Writing a function template with a variable number of arguments."
+    << "\n-----------------------------------------------------------------------------\n";
 
-      auto lnums = std::list<int>{ 1, 2, 3, 4, 5 };
-      auto l = funclib::mapf([](int const i) { return i * i; }, lnums);
-      // l = {1, 4, 9, 16, 25}
+  using namespace std::literals;
 
-      auto amounts = std::array<double, 5>{ 10.42, 2.50, 100.0, 23.75, 12.99 };
-      auto a = funclib::mapf(fround<>(), amounts);
-      // a = {10.0, 3.0, 100.0, 24.0, 13.0}
+  auto s1 = add(1, 2, 3, 4, 5);
+  // s1 = 15
+  std::cout << s1 << std::endl;
 
-      auto words
-        = std::map<std::string, int>{ { "one", 1 }, { "two", 2 }, { "three", 3 } };
-      auto m = funclib::mapf(
-        [](std::pair<std::string, int> const kvp) {
-          return std::make_pair(funclib::mapf(toupper, kvp.first), kvp.second);
-        },
-        words);
-      // m = { {"ONE", 1}, {"TWO", 2}, {"THREE", 3} }
-
-      auto priorities = std::queue<int>();
-      priorities.push(10);
-      priorities.push(20);
-      priorities.push(30);
-      priorities.push(40);
-      priorities.push(50);
-      auto p = funclib::mapf([](int const i) { return i > 30 ? 2 : 1; }, priorities);
-      // p = {1, 1, 1, 2, 2}
-    }
-
-    {
-      auto vnums = std::vector<int>{ 0, 2, -3, 5, -1, 6, 8, -4, 9 };
-      auto s1 = funclib::foldl([](const int s, const int n) { return s + n; }, vnums,
-                               0);                       // s1 = 22
-      auto s2 = funclib::foldl(std::plus<>(), vnums, 0); // s2 = 22
-
-      auto s3 = funclib::foldr([](const int s, const int n) { return s + n; }, vnums,
-                               0);                       // s3 = 22
-      auto s4 = funclib::foldr(std::plus<>(), vnums, 0); // s4 = 22
-
-      auto texts = std::vector<std::string>{ "hello"s, " "s, "world"s, "!"s };
-      auto txt1 = funclib::foldl(
-        [](std::string const& s, std::string const& n) { return s + n; }, texts, ""s);
-      // txt1 = "hello world!"
-      auto txt2 = funclib::foldr(
-        [](std::string const& s, std::string const& n) { return s + n; }, texts, ""s);
-      // txt2 = "!world hello"
-
-      char chars[] = { 'c', 'i', 'v', 'i', 'c' };
-      auto str1 = funclib::foldl(std::plus<>(), chars, ""s); // str1 = "civic"
-      auto str2 = funclib::foldr(std::plus<>(), chars, ""s); // str2 = "civic"
-
-      auto words
-        = std::map<std::string, int>{ { "one", 1 }, { "two", 2 }, { "three", 3 } };
-      auto count = funclib::foldl(
-        [](int const s, std::pair<std::string, int> const kvp) { return s + kvp.second; },
-        words, 0);
-
-      auto q = std::queue<int>{};
-      q.push(1);
-      q.push(2);
-      q.push(3);
-      q.push(4);
-      q.push(5);
-      auto sum = funclib::foldl(std::plus<>(), q, 0); // sum = 15
-    }
-
-    {
-      auto vnums = std::vector<int>{ 0, 2, -3, 5, -1, 6, 8, -4, 9 };
-
-      auto s = funclib::foldl(
-        std::plus<>(),
-        funclib::mapf([](int const i) { return i + i; },
-                      funclib::mapf([](int const i) { return std::abs(i); }, vnums)),
-        0);
-      // s = 76
-    }
-  }
+  auto s2 = add("hello"s, " "s, "world"s, "!"s);
+  // s2 = "hello world!"
+  std::cout << s2 << std::endl;
+}
 }
--- Chapter03/recipe_3_07.h
+++ Chapter03/recipe_3_07.h
@@ -1,80 +1,112 @@
 #pragma once
 
+#include "funclib.h"
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
 #include <string>
+#include <vector>
+#include <cmath>
 
 namespace recipe_3_07 {
-  template <typename T>
-  T sum(T value)
-  {
-    return value;
-  }
+  using namespace std::string_literals;
 
-  template <typename T, typename... Ts>
-  T sum(T head, Ts... tail)
-  {
-    return head + sum(tail...);
-  }
-
-  template <typename F, typename T1, typename T2>
-  auto fold(F&& f, T1 v1, T2 v2)
-  {
-    return f(v1, v2);
-  }
-
-  template <typename F, typename T, typename... Ts>
-  auto fold(F&& f, T head, Ts... tail)
-  {
-    return f(head, fold(std::forward<F>(f), tail...));
-  }
-
-  template <typename T>
-  bool is_fibonacci(T v1, T v2, T v3)
-  {
-    return v1 + v2 == v3;
-  }
-  /*
-template <typename T>
-bool is_fibonacci(T v1, T v2, T v3)
-{
-   return v1 + v2 == v3;
-}
-
-template <typename T, typename... Ts>
-bool is_fibonacci(T v1, T v2, T v3, Ts... args)
-{
-   return v1 + v2 == v3 && is_fibonacci(args...);
-}*/
-
-  template <typename T>
-  bool is_any(T value, T elem)
-  {
-    return value == elem;
-  }
-
-  template <typename T, typename... Ts>
-  bool is_any(T value, T first, Ts... rest)
-  {
-    return value == first || is_any(value, rest...);
-  }
+  template <class T = double>
+  struct fround {
+    typename std::enable_if<std::is_floating_point<T>::value, T>::type
+    operator()(const T& value) const
+    {
+      return std::round(value);
+    }
+  };
 
   void execute()
   {
-    using namespace std::string_literals;
+    std::cout
+      << "\nRecipe 3.07: ."
+      << "\n----------------------------------------------------------\n";
 
     {
-      auto n = sum(1, 2, 3, 4, 5);
-      auto s = sum("hello"s, " "s, "world"s, "!"s);
+      auto vnums = std::vector<int>{ 0, 2, -3, 5, -1, 6, 8, -4, 9 };
+      auto r = funclib::mapf([](int const i) { return std::abs(i); }, vnums);
+      // r = {0, 2, 3, 5, 1, 6, 8, 4, 9}
+
+      auto lnums = std::list<int>{ 1, 2, 3, 4, 5 };
+      auto l = funclib::mapf([](int const i) { return i * i; }, lnums);
+      // l = {1, 4, 9, 16, 25}
+
+      auto amounts = std::array<double, 5>{ 10.42, 2.50, 100.0, 23.75, 12.99 };
+      auto a = funclib::mapf(fround<>(), amounts);
+      // a = {10.0, 3.0, 100.0, 24.0, 13.0}
+
+      auto words
+        = std::map<std::string, int>{ { "one", 1 }, { "two", 2 }, { "three", 3 } };
+      auto m = funclib::mapf(
+        [](std::pair<std::string, int> const kvp) {
+          return std::make_pair(funclib::mapf(toupper, kvp.first), kvp.second);
+        },
+        words);
+      // m = { {"ONE", 1}, {"TWO", 2}, {"THREE", 3} }
+
+      auto priorities = std::queue<int>();
+      priorities.push(10);
+      priorities.push(20);
+      priorities.push(30);
+      priorities.push(40);
+      priorities.push(50);
+      auto p = funclib::mapf([](int const i) { return i > 30 ? 2 : 1; }, priorities);
+      // p = {1, 1, 1, 2, 2}
     }
 
     {
-      auto n = fold(std::plus<>(), 1, 2, 3, 4, 5);
-      auto s = fold(std::plus<>(), "hello"s, " "s, "world"s, "!"s);
+      auto vnums = std::vector<int>{ 0, 2, -3, 5, -1, 6, 8, -4, 9 };
+      auto s1 = funclib::foldl([](const int s, const int n) { return s + n; }, vnums,
+                               0);                       // s1 = 22
+      auto s2 = funclib::foldl(std::plus<>(), vnums, 0); // s2 = 22
+
+      auto s3 = funclib::foldr([](const int s, const int n) { return s + n; }, vnums,
+                               0);                       // s3 = 22
+      auto s4 = funclib::foldr(std::plus<>(), vnums, 0); // s4 = 22
+
+      auto texts = std::vector<std::string>{ "hello"s, " "s, "world"s, "!"s };
+      auto txt1 = funclib::foldl(
+        [](std::string const& s, std::string const& n) { return s + n; }, texts, ""s);
+      // txt1 = "hello world!"
+      auto txt2 = funclib::foldr(
+        [](std::string const& s, std::string const& n) { return s + n; }, texts, ""s);
+      // txt2 = "!world hello"
+
+      char chars[] = { 'c', 'i', 'v', 'i', 'c' };
+      auto str1 = funclib::foldl(std::plus<>(), chars, ""s); // str1 = "civic"
+      auto str2 = funclib::foldr(std::plus<>(), chars, ""s); // str2 = "civic"
+
+      auto words
+        = std::map<std::string, int>{ { "one", 1 }, { "two", 2 }, { "three", 3 } };
+      auto count = funclib::foldl(
+        [](int const s, std::pair<std::string, int> const kvp) { return s + kvp.second; },
+        words, 0);
+
+      auto q = std::queue<int>{};
+      q.push(1);
+      q.push(2);
+      q.push(3);
+      q.push(4);
+      q.push(5);
+      auto sum = funclib::foldl(std::plus<>(), q, 0); // sum = 15
     }
 
     {
-      auto i1 = is_any(42, 1, 23, 76, 44, 5);
+      auto vnums = std::vector<int>{ 0, 2, -3, 5, -1, 6, 8, -4, 9 };
 
-      auto a = 0;
+      auto s = funclib::foldl(
+        std::plus<>(),
+        funclib::mapf([](int const i) { return i + i; },
+                      funclib::mapf([](int const i) { return std::abs(i); }, vnums)),
+        0);
+      // s = 76
     }
   }
 }
